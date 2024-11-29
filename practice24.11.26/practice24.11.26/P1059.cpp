d694b68e463050bff519b0194c145d9bb01815cb
zhaoshaoyu <1499623600@qq.com>
2024-11-29 23:26:39 +0800

    24.11.29

--- practice24.11.26/practice24.11.26/P1059.cpp
+++ practice24.11.26/practice24.11.26/P1059.cpp
@@ -1,22 +1,22 @@
-#include<iostream>
-#include<algorithm>
-#include<cstring>
-using namespace std;
-int arr[105];
-int main() {
-	int n;
-	cin >> n;
-	for (int i = 1; i <= n; i++) cin >> arr[i];
-	sort(arr + 1, arr + n + 1);
-	int count = 0;
-	for (int i = 1; i < n; i++) {
-		if (arr[i] == arr[i + 1]) {
-			memcpy(arr + i, arr + i + 1, sizeof(int) * (n - i));
-			n--;
-			i--;
-		}
-	}
-	cout << n  << endl;
-	for (int i = 1; i <= n ; i++) cout << arr[i]<<" ";
-	return 0;
-}
+//#include<iostream>
+//#include<algorithm>
+//#include<cstring>
+//using namespace std;
+//int arr[105];
+//int main() {
+//	int n;
+//	cin >> n;
+//	for (int i = 1; i <= n; i++) cin >> arr[i];
+//	sort(arr + 1, arr + n + 1);
+//	int count = 0;
+//	for (int i = 1; i < n; i++) {
+//		if (arr[i] == arr[i + 1]) {
+//			memcpy(arr + i, arr + i + 1, sizeof(int) * (n - i));
+//			n--;
+//			i--;
+//		}
+//	}
+//	cout << n  << endl;
+//	for (int i = 1; i <= n ; i++) cout << arr[i]<<" ";
+//	return 0;
+//}
